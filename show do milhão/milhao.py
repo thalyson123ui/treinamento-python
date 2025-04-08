bd17cc7aa453a4cfaa6d3a4393320945a9af39fc
thalyson123ui <69772569+thalyson123ui@users.noreply.github.com>
2025-04-08 11:10:13 -0300

    atualização

---
+++ show do milhão/milhao.py
@@ -0,0 +1,13 @@
+import pygame
+import sys
+import random
+
+# Inicializa o Pygame
+pygame.init()
+
+# tamanho da tela
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Show do Milhão")
+
+# Cores
